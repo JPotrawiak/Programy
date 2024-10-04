a1b993df833dc87199c1ebfcb98f41ddfa984b70
JPotrawiak <potrawiakjakub@gmail.com>
2024-10-04 18:30:43 +0200

    new project

--- 1. tablice,listy/obj/Release/net8.0/1. tablice,listy.AssemblyInfo.cs
+++ 1. tablice,listy/obj/Release/net8.0/1. tablice,listy.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("1. tablice,listy")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c710a64d100876cf3d77ef2238f906fc81e5910")]
 [assembly: System.Reflection.AssemblyProductAttribute("1. tablice,listy")]
